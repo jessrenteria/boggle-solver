2d01f49602ec9ef6d6b63c28d7d349d83bcb3158
jessrenteria <jvr35@cornell.edu>
2016-09-06 11:03:49 -0400

    added support for 'qu'

--- src/boggler.py
+++ src/boggler.py
@@ -21,28 +21,26 @@ def getAllWords(grid, t):
     words = [set() for _ in range(len(grid) * len(grid[0]))]
 
     def getAllWordsHelper(used, t, s, r, c):
-        if used[r][c]:
-            return
         used = [list(used[i]) for i in range(len(used))]
         used[r][c] = True
-        t = t.getNode(grid[r][c])
+        elem = grid[r][c]
+        while t != None and elem != "":
+            t = t.getNode(elem[0])
+            s += elem[0]
+            elem = elem[1:]
         if t == None:
             return
-        s += grid[r][c]
-
         if t.is_word:
             words[len(s) - 1].add(s)
-
         for y in range(max(0, r - 1), min(len(grid), r + 2)):
             for x in range(max(0, c - 1), min(len(grid[0]), c + 2)):
-                getAllWordsHelper(used, t, s, y, x)
+                if not used[y][x]:
+                    getAllWordsHelper(used, t, s, y, x)
 
     used = [[False] * len(grid[0]) for _ in range(len(grid))]
-
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             getAllWordsHelper(used, t, "", r, c)
-
     return words
 
 def writeWords(found_file, words):
